92375697a73ac9211034b9b811cf83b22d5a7a67
Filip Lönnqvist <fl223km@student.lnu.se>
2025-11-24 13:42:01 +0100

    Finished isCorrectFormat and isValidMonth tests.

--- js/practice/practice_write_mocked_tests/tests/SwedishSocialSecurityNumber.test.js
+++ js/practice/practice_write_mocked_tests/tests/SwedishSocialSecurityNumber.test.js
@@ -1,20 +1,50 @@
-import { SwedishSocialSecurityNumber } from '../src/correct/SwedishSocialSecurityNumber'; 
+import { SwedishSocialSecurityNumber } from '../src/correct/SwedishSocialSecurityNumber';
+import { beforeEach, expect, jest } from '@jest/globals';
 
 
 
 //NOTE THESE TESTS SHOULD NOT BE DEPENDENT ON SSNHelper BUT USE MOCKING
 describe('SwedishSocialSecurityNumber Tests', () => {
-    //put constants here to increase readability
+    let mockHelper
+
+    beforeEach(() => {
+        mockHelper = {
+            isCorrectLength: jest.fn(),
+            isCorrectFormat: jest.fn(),
+            isValidMonth: jest.fn()
+        }
+    })
 
     test('Constructor should throw an error when social security number is too short', () => {
-        const mockHelper = {
-            isCorrectLength: () => false
-        }
+        mockHelper.isCorrectLength.mockReturnValue(false)
 
         function createSSN() {
-            new SwedishSocialSecurityNumber('800231-1234', mockHelper)
+            new SwedishSocialSecurityNumber('770722-3322 ', mockHelper)
         }
 
         expect(createSSN).toThrow('To short, must be 11 characters')
+        expect(mockHelper.isCorrectLength).toHaveBeenCalled()
+    })
+
+    test('Constructor should throw an error when social security number has an incorrect format', () => {
+        mockHelper.isCorrectFormat.mockReturnValue(false)
+
+        function createSSN() {
+            new SwedishSocialSecurityNumber('770722-XXXX ', mockHelper)
+        }
+
+        expect(createSSN).toThrow('Incorrect format, must be: YYMMDD-XXXX')
+        expect(mockHelper.isCorrectFormat).toHaveBeenCalled()
+    })
+
+    test('Constructor should throw an error when social security number has an invalid month', () => {
+        mockHelper.isValidMonth.mockReturnValue(false)
+
+        function createSSN() {
+            new SwedishSocialSecurityNumber('770722-3322', mockHelper)
+        }
+
+        expect(createSSN).toThrow('Invalid month in SSN')
+        expect(mockHelper.isValidMonth).toHaveBeenCalledWith('07')
     })
 });
