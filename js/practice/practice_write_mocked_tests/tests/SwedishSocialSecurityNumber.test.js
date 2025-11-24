c6e13cbe9328002cd8d6cf987c1e0dc57591f452
Filip Lönnqvist <fl223km@student.lnu.se>
2025-11-24 11:47:13 +0100

    First mock created.

--- js/practice/practice_write_mocked_tests/tests/SwedishSocialSecurityNumber.test.js
+++ js/practice/practice_write_mocked_tests/tests/SwedishSocialSecurityNumber.test.js
@@ -6,9 +6,15 @@ import { SwedishSocialSecurityNumber } from '../src/correct/SwedishSocialSecurit
 describe('SwedishSocialSecurityNumber Tests', () => {
     //put constants here to increase readability
 
-    test('replace this test with one of your own', () => {
-        expect(true).toBe(true);
-    });
+    test('Constructor should throw an error when social security number is too short', () => {
+        const mockHelper = {
+            isCorrectLength: () => false
+        }
 
-    //Add your tests here
+        function createSSN() {
+            new SwedishSocialSecurityNumber('800231-1234', mockHelper)
+        }
+
+        expect(createSSN).toThrow('To short, must be 11 characters')
+    })
 });
